b49a6833c4f61a97600836843819a56704802093
Candui <serapup2@mail.ru>
2022-07-02 15:02:24 +0300

    added cart dropdown

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,19 +1,19 @@
-import './product-card.styles.scss'
+import './product-card.styles.scss';
+
 import Button from '../button/button.component';
 
-const ProductCard = ({ product })=>{
-const { name,price,imageUrl } = product;
-return(
-    <div className='Product-card-container'>
-    <img src={imageUrl} alt={'${name}'}/>
-    <div className='footer'>
-    <span className='Name'>{name}</span>
-    <span className='Price'>{price}</span>
+const ProductCard = ({ product }) => {
+  const { name, price, imageUrl } = product;
+  return (
+    <div className='product-card-container'>
+      <img src={imageUrl} alt={`${name}`} />
+      <div className='footer'>
+        <span className='name'>{name}</span>
+        <span className='price'>{price}</span>
+      </div>
+      <Button buttonType='inverted'>Add to card</Button>
     </div>
-    <Button buttonType='inverted'>Add to card</Button>
-    </div>
-    );
-
+  );
 };
 
 export default ProductCard;
