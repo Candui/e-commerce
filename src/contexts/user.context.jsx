3614f0257a03b85f3436c05e4e5e1cf9c2d4dc2c
Candui <serapup2@mail.ru>
2022-07-21 13:59:27 +0300

    asd

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -10,36 +10,21 @@ export const UserContext = createContext({
   currentUser: null,
 });
 
-const userReducer = (type, action)={
-  const {type,payload} = action; 
-  
-  switch(type){
-    case `SET_CURRENT_USER`:
-      return{
-        currentUser:payload
-      }
-    
-    
-    default: 
-    throw new Error(`Unhandled type ${type} in userReducer`)
-  }
 
-}
+
 
 export const UserProvider = ({ children }) => {
-  // const [currentUser, setCurrentUser] = useState(null);
-  const value = { currentUser, setCurrentUser };
+ 
 
   useEffect(() => {
     const unsubscribe = onAuthStateChangedListener((user) => {
       if (user) {
         createUserDocumentFromAuth(user);
       }
-      setCurrentUser(user);
+  
     });
 
     return unsubscribe;
   }, []);
 
-  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
